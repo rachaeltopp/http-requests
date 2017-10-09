01d674afdddd27d82b4c6a1d4e14c7d08fc454ae
Rachael Topp <topp@ualberta.ca>
2017-10-09 15:28:03 +0000

    added options as a paramater

---
+++ getAndPrintHTML2.js
@@ -0,0 +1,25 @@
+var https = require('https');
+
+function getAndPrintHTML (options) {
+
+  https.get(options, function (response) {
+    response.setEncoding('utf8');
+    response.on('data', function (chunk) {
+      var buffer = '';
+      if (chunk) {
+        buffer += chunk;
+      };
+      console.log(buffer + '\n');
+    });
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+  });
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML(requestOptions);
