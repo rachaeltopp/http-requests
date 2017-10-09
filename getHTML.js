90335d485aa2a9118b032fa5523cdce72cf218be
Rachael Topp <topp@ualberta.ca>
2017-10-09 15:38:52 +0000

    added callback function for what to do with data

---
+++ getHTML.js
@@ -0,0 +1,28 @@
+var https = require('https');
+
+function getHTML (options, callback) {
+
+  https.get(options, function (response) {
+    response.setEncoding('utf8');
+    response.on('data', function (chunk) {
+      var buffer = '';
+      if (chunk) {
+        buffer += chunk;
+      };
+      callback(buffer);
+    });
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+  });
+}
+function printHTML (html) {
+  console.log(html);
+};
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
